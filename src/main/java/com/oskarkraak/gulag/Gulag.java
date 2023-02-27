52cac999e6f777be1c5590e13fc6228b30e112c2
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-02-27 17:50:18 +0100

    Create Minecraft mod

---
+++ src/main/java/com/oskarkraak/gulag/Gulag.java
@@ -0,0 +1,12 @@
+package com.oskarkraak.gulag;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Gulag implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+
+}
