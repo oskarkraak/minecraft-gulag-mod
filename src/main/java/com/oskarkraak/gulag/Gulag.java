4136e32bb0bd8ca76531a5dd0881cfdb071993c8
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-03-07 12:32:59 +0100

    Add new ChunkGenerator

--- src/main/java/com/oskarkraak/gulag/Gulag.java
+++ src/main/java/com/oskarkraak/gulag/Gulag.java
@@ -4,11 +4,15 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xyz.nucleoid.fantasy.Fantasy;
@@ -18,25 +22,33 @@ import xyz.nucleoid.fantasy.RuntimeWorldHandle;
 public class Gulag implements DedicatedServerModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("Gulag");
+
     public static MinecraftServer server;
 
     @Override
     public void onInitializeServer() {
         ServerLifecycleEvents.SERVER_STARTED.register(this::loadGulag);
     }
-    
+
     private void loadGulag(MinecraftServer server) {
         this.server = server;
 
-        RegistryKey<DimensionType> overworldDimensionTypeKey = RegistryKey.of(RegistryKeys.DIMENSION_TYPE, new Identifier("minecraft", "the_nether"));
-        ServerWorld world = server.getOverworld();
+        RegistryKey<DimensionType> overworldDimensionTypeKey = RegistryKey.of(
+                RegistryKeys.DIMENSION_TYPE, new Identifier("minecraft", "overworld"));
+        BiomeSource bs = server.getOverworld().getChunkManager().getChunkGenerator().getBiomeSource();
+        RegistryKey<ChunkGeneratorSettings> cgsKey = ChunkGeneratorSettings.OVERWORLD;
+        RegistryEntry<ChunkGeneratorSettings> cgsEntry =
+                server.getRegistryManager().get(RegistryKeys.CHUNK_GENERATOR_SETTINGS).getEntry(cgsKey).orElseThrow();
+        ChunkGenerator cg = new NoiseChunkGenerator(bs, cgsEntry);
         RuntimeWorldConfig worldConfig = new RuntimeWorldConfig()
                 .setDimensionType(overworldDimensionTypeKey)
                 .setDifficulty(Difficulty.HARD)
-                .setGenerator(world.getChunkManager().getChunkGenerator());
-        Identifier id = new Identifier("gulag", "overworld");
-        RuntimeWorldHandle worldHandle = Fantasy.get(server).getOrOpenPersistentWorld(id, worldConfig);
-        LOGGER.info("Loaded dimension gulag:overworld");
+                .setGenerator(cg)
+                .setSeed(100L);
+        RuntimeWorldHandle h = Fantasy.get(Gulag.server).openTemporaryWorld(worldConfig);
+//        Identifier id = new Identifier("gulag", "overworld");
+//        RuntimeWorldHandle worldHandle = Fantasy.get(server).getOrOpenPersistentWorld(id, worldConfig);
+//        LOGGER.info("Loaded dimension gulag:overworld");
     }
 
 }
