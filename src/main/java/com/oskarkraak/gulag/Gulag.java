0dc5c58e4b08684a1434ce2fb047f050db00613c
Moritz Beier <90337771+moritzbeier@users.noreply.github.com>
2023-03-08 14:31:06 +0100

    change environment to *

--- src/main/java/com/oskarkraak/gulag/Gulag.java
+++ src/main/java/com/oskarkraak/gulag/Gulag.java
@@ -1,25 +1,25 @@
 package com.oskarkraak.gulag;
 
-import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.Difficulty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Gulag implements DedicatedServerModInitializer {
+public class Gulag implements ModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("Gulag");
 
     public static MinecraftServer server;
 
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         ServerLifecycleEvents.SERVER_STARTED.register(this::loadGulag);
     }
 
     private void loadGulag(MinecraftServer server) {
-        this.server = server;
+        Gulag.server = server;
         new Overworld(server, "gulag", 100L, Difficulty.HARD);
         LOGGER.info("Loaded dimension gulag:overworld");
         new End(server, "gulag", 100L, Difficulty.HARD);
