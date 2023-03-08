69d769bd4a86ed4434ad76ea1baccb44fa3a5853
Moritz Beier <90337771+moritzbeier@users.noreply.github.com>
2023-03-08 15:55:33 +0100

    teleport player to gulag:overworld on respawn

--- src/main/java/com/oskarkraak/gulag/Gulag.java
+++ src/main/java/com/oskarkraak/gulag/Gulag.java
@@ -1,9 +1,13 @@
 package com.oskarkraak.gulag;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.TeleportTarget;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,19 +16,27 @@ public class Gulag implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("Gulag");
 
     public static MinecraftServer server;
+    private Overworld overworld;
+    private Nether nether;
+    private End end;
 
     @Override
     public void onInitialize() {
         ServerLifecycleEvents.SERVER_STARTED.register(this::loadGulag);
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
+            BlockPos spawnPos = overworld.asWorld().getSpawnPos();
+            TeleportTarget target = new TeleportTarget(spawnPos.toCenterPos(), newPlayer.getVelocity(), newPlayer.getYaw(), newPlayer.getPitch());
+            FabricDimensions.teleport(newPlayer, overworld.asWorld(), target);
+        });
     }
 
     private void loadGulag(MinecraftServer server) {
         Gulag.server = server;
-        new Overworld(server, "gulag", 100L, Difficulty.HARD);
+        overworld = new Overworld(server, "gulag", 100L, Difficulty.HARD);
         LOGGER.info("Loaded dimension gulag:overworld");
-        new Nether(server, "gulag", 100L, Difficulty.HARD);
+        nether = new Nether(server, "gulag", 100L, Difficulty.HARD);
         LOGGER.info("Loaded dimension gulag:the_nether");
-        new End(server, "gulag", 100L, Difficulty.HARD);
+        end = new End(server, "gulag", 100L, Difficulty.HARD);
         LOGGER.info("Loaded dimension gulag:the_end");
     }
 
--- src/main/java/com/oskarkraak/gulag/VanillaWorld.java
+++ src/main/java/com/oskarkraak/gulag/VanillaWorld.java
@@ -56,4 +56,12 @@ public class VanillaWorld {
         worldHandle.delete();
     }
 
+    public ServerWorld asWorld(){
+        return worldHandle.asWorld();
+    }
+
+    public RegistryKey<World> getRegistryKey(){
+        return worldHandle.getRegistryKey();
+    }
+
 }
