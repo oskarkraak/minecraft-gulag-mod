476e2ce177d1f06968d87dabb503f864e74c5f84
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-04-16 17:37:03 +0200

    Create new gulag after winning

--- src/main/java/com/oskarkraak/gulag/Gulag.java
+++ src/main/java/com/oskarkraak/gulag/Gulag.java
@@ -3,6 +3,7 @@ package com.oskarkraak.gulag;
 import com.oskarkraak.gulag.mixin.ServerPlayerEntityInvoker;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.entity.Entity;
@@ -20,6 +21,8 @@ import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Random;
+
 public class Gulag implements ModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("Gulag");
@@ -33,31 +36,21 @@ public class Gulag implements ModInitializer {
     public static Overworld overworld;
     public static Nether nether;
     public static End end;
+    public static boolean isLoaded;
 
-    @Override
-    public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTED.register(this::loadGulag);
-        ServerPlayerEvents.AFTER_RESPAWN.register(this::onRespawn);
+    private static void loadGulag(long seed) {
+        overworld = new Overworld(server, "gulag", seed, Difficulty.HARD);
+        nether = new Nether(server, "gulag", seed, Difficulty.HARD);
+        end = new End(server, "gulag", seed, Difficulty.HARD);
+        Gulag.isLoaded = true;
+        LOGGER.info("Loaded gulag with seed " + seed);
     }
 
-    private void loadGulag(MinecraftServer server) {
-        Gulag.server = server;
-        overworld = new Overworld(server, "gulag", 100L, Difficulty.HARD);
-        LOGGER.info("Loaded dimension gulag:overworld");
-        nether = new Nether(server, "gulag", 100L, Difficulty.HARD);
-        LOGGER.info("Loaded dimension gulag:the_nether");
-        end = new End(server, "gulag", 100L, Difficulty.HARD);
-        LOGGER.info("Loaded dimension gulag:the_end");
+    private static long getRandomSeed() {
+        return new Random().nextLong();
     }
 
-    private void onRespawn(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
-        if (!alive) {
-            respawnInGulag(newPlayer);
-            sendInfoMessage(newPlayer);
-        }
-    }
-
-    private void respawnInGulag(ServerPlayerEntity player) {
+    private static void respawnInGulag(ServerPlayerEntity player) {
         BlockPos bestSpawnPos = overworld.asWorld().getChunkManager().getNoiseConfig().getMultiNoiseSampler()
                 .findBestSpawnPosition();
         BlockPos spawnPos = SpawnLocating.findServerSpawnPoint(overworld.asWorld(), new ChunkPos(bestSpawnPos));
@@ -65,15 +58,65 @@ public class Gulag implements ModInitializer {
         FabricDimensions.teleport(player, overworld.asWorld(), target);
     }
 
-    private void sendInfoMessage(ServerPlayerEntity player) {
+    private static void sendInfoMessage(ServerPlayerEntity player) {
         executeCommand(RESPAWN_MESSAGE_TIME);
         executeCommand(RESPAWN_MESSAGE_TITLE);
         executeCommand(RESPAWN_MESSAGE_SUBTITLE);
         player.sendMessage(Text.of(RESPAWN_MESSAGE_CHAT));
     }
 
-    private void executeCommand(String command) {
+    private static void executeCommand(String command) {
         server.getCommandManager().executeWithPrefix(server.getCommandSource(), command);
+    }
+
+    private static boolean shouldDeleteGulag() {
+        boolean isEmtpy = countPlayersInGulag() == 0;
+        boolean enderDragonDefeated = end.asWorld().getEnderDragonFight().hasPreviouslyKilled();
+        return isEmtpy && enderDragonDefeated;
+    }
+
+    private static int countPlayersInGulag() {
+        int playersInOverworld = overworld.asWorld().getPlayers().size();
+        int playersInNether = nether.asWorld().getPlayers().size();
+        int playersInEnd = end.asWorld().getPlayers().size();
+        return playersInOverworld + playersInNether + playersInEnd;
+    }
+
+    private static void deleteGulag() {
+        Gulag.isLoaded = false;
+        long seed = overworld.getSeed();
+        overworld.delete();
+        nether.delete();
+        end.delete();
+        LOGGER.info("Deleted gulag with seed " + seed);
+    }
+
+    @Override
+    public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStart);
+        ServerPlayerEvents.AFTER_RESPAWN.register(this::onRespawn);
+        ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register(this::onPlayerChangeWorld);
+    }
+
+    private void onServerStart(MinecraftServer server) {
+        Gulag.server = server;
+        loadGulag(getRandomSeed());
+    }
+
+    private void onRespawn(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
+        if (!alive) {
+            if (!Gulag.isLoaded) {
+                loadGulag(getRandomSeed());
+            }
+            respawnInGulag(newPlayer);
+            sendInfoMessage(newPlayer);
+        }
+    }
+
+    private void onPlayerChangeWorld(ServerPlayerEntity player, ServerWorld origin, ServerWorld destination) {
+        if (shouldDeleteGulag()) {
+            deleteGulag();
+        }
     }
 
     /**
